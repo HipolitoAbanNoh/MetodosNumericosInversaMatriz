d2e841492a9f7cd45180f41760990020a56cf34e
DavidDanielYam <39715613+DavidDanielYam@users.noreply.github.com>
2019-04-03 10:58:29 -0600

    Inicio del Proyecto

---
+++ Tema3/src/Tema3/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tema3;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frmPrincipal frm = new frmPrincipal();
+        frm.setVisible(true);
+    }
+    
+}
